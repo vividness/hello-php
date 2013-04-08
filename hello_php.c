f3827ff5fcc78a00718dd643b83abedcc53ed2a6
vivic <vivic@ubuntu.(none)>
2013-04-07 18:31:28 -0700

    Removing the header file in trade for simplicity

--- hello_php.c
+++ hello_php.c
@@ -1,16 +1,30 @@
-//include php api
+/* 
+ * Include the php api so we can talk to it
+ */
 #include <php.h>
 
-//include our header, the "php_" prefix is mandatory
-#include "php_hello_php.h"
+/*
+ * Module constants
+ */
+#define PHP_HELLO_PHP_EXTNAME "hello_php"
+#define PHP_HELLO_PHP_VERSION "0.0.1"
 
-//define functs we want to add 
+/*
+ * Here we declare our function
+ */
+PHP_FUNCTION(hello_php);
+
+/*
+ * Introduce our function to the PHP API
+ */  
 zend_function_entry hello_php_functions[] = {
     PHP_FE(hello_php, NULL)
     {NULL, NULL, NULL}
 };
 
-//module entry definitions
+/*
+ * Info about our module
+ */
 zend_module_entry hello_php_module_entry = {
     STANDARD_MODULE_HEADER,
     PHP_HELLO_PHP_EXTNAME,
@@ -24,9 +38,17 @@ zend_module_entry hello_php_module_entry = {
     STANDARD_MODULE_PROPERTIES
 };
 
-//install module
+/*
+ * This macro will output additional C 
+ * code necessary for making this ext 
+ * dynamically loadable
+ */
 ZEND_GET_MODULE(hello_php)
 
+/**
+ * Hooray! Here's our function definition!
+ */
 PHP_FUNCTION(hello_php) {
     php_printf("Hello PHP!\n");
 }
+
